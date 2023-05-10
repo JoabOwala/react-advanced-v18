3d3820cb317bd4952420be5c15d0958dccae2bad
Joab Owala <owalajoab@gmail.com>
2023-05-10 10:18:11 +0300

    remove using filter method

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -3,18 +3,28 @@ import { data } from '../../../data';
 
 const UseStateArray = () => {
   const [people,setPeople] = React.useState(data)
-  return <>
-  {
-    people.map((person)=> {
+
+  const removeItem = (id) =>{
+    let newPeople = people.filter((person) => person.id !== id)
+    console.log(newPeople)
+    setPeople(newPeople)
+  }
+  return (
+  <>
+
+    {people.map((person)=> {
       const {id , name} = person
       return (
         <div key={id} className='item'>
         <h4>{name}</h4>
+        <button className='btn' onClick={()=> removeItem(id)}>remove</button>
         </div>
     )
-    })
-  }
+  })}
+  <button className='btn' onClick={() => setPeople([])}>clear Items</button>
+    
   </>
+  )
 };
 
 export default UseStateArray;
